a7e5b2299ade9b52799baf24e08d2c6ba214265e
Jonathan Martins <web@jonathanmartins.com.br>
2022-11-02 08:06:27 -0300

    capacitor-bluetooth-serial installation

--- src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
+++ src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
+import com.bluetoothserial.plugin.BluetoothSerial;
 
 import java.util.ArrayList;
 
@@ -16,12 +17,11 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(BluetoothSerial.class);
     }});
 
-    if (BuildConfig.DEBUG) {
-      EnableHttpsSelfSigned.enable(this.bridge);
-    }
-      
-              
+//    if (BuildConfig.DEBUG) {
+//      EnableHttpsSelfSigned.enable(this.bridge);
+//    }
   }
 }
